a4a5076203005ae41ced8e7893dd3956c7f06b59
Ellord Tayag <ellord.tayag@genero.com>
2025-10-29 20:51:19 +1100

    Update themes

--- nvim-config/lua/custom/plugins/auto-dark-mode.lua
+++ nvim-config/lua/custom/plugins/auto-dark-mode.lua
@@ -3,23 +3,30 @@ return {
   opts = {
     update_interval = 1000,
     set_dark_mode = function()
-      vim.api.nvim_set_option_value('background', 'dark', {})
-      -- require('catppuccin').setup {
-      --   flavour = 'mocha', -- latte, frappe, macchiato, mocha
-      -- }
       -- Load the colorscheme here
-      vim.cmd 'colorscheme nightfox'
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+      }
+      vim.cmd.colorscheme 'catppuccin'
+
+      -- Force transparent background
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
     end,
     set_light_mode = function()
-      vim.api.nvim_set_option_value('background', 'light', {})
-      -- require('catppuccin').setup {
-      --   flavour = 'latte', -- latte, frappe, macchiato, mocha
-      -- }
-      -- Load the colorscheme here
-      vim.cmd 'colorscheme dawnfox'
+      require('catppuccin').setup {
+        flavour = 'latte', -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+      }
+      vim.cmd.colorscheme 'catppuccin'
+
+      -- Force transparent background
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE' })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE' })
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
--- nvim-config/lua/custom/plugins/catppuccin.lua
+++ nvim-config/lua/custom/plugins/catppuccin.lua
@@ -1,1 +1,5 @@
-return { 'EdenEast/nightfox.nvim', name = 'nightfox', priority = 1000 }
+-- return { 'EdenEast/nightfox.nvim', name = 'nightfox', priority = 1000 }
+
+return { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, opts = {
+  flavour = 'mocha',
+} }

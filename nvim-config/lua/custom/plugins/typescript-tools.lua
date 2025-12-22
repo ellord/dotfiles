787f9cd3c34b0fcca1680be6a85b1440e08d9e88
Ellord Tayag <ellord.tayag@genero.com>
2025-12-22 15:00:37 +1100

    Fix TS tools

--- nvim-config/lua/custom/plugins/typescript-tools.lua
+++ nvim-config/lua/custom/plugins/typescript-tools.lua
@@ -2,9 +2,13 @@ return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {
-    root_dir = require('lspconfig').util.root_pattern('package.json', 'tsconfig.json'),
-    single_file_support = false,
+    -- Allow single file support to work outside projects
+    single_file_support = true,
     settings = {
+      -- Let the plugin auto-detect tsserver from local node_modules or global installation
+      tsserver_path = nil,
+      separate_diagnostic_server = true,
+      publish_diagnostic_on = "insert_leave",
       typescript = {
         completion = {
           includeCompletionsForModuleExports = true,

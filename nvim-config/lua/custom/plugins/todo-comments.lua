522cd57aed10a7ae3fd64105e9496393e04f39a2
Ellord Tayag <ellord@gmail.com>
2026-04-02 18:53:15 +1100

    Fix todo-comments light mode

--- nvim-config/lua/custom/plugins/todo-comments.lua
+++ nvim-config/lua/custom/plugins/todo-comments.lua
@@ -11,6 +11,7 @@ return {
       MARK = { icon = '󰃀 ', color = 'hint' },
     },
     highlight = {
+      keyword = 'bg',
       pattern = {
         [[.*\W(KEYWORDS)\s*:]],
         [[.*\W(KEYWORDS)\s]],
@@ -26,6 +27,48 @@ return {
 
     local marks = require 'custom.marks'
     local todo_comments = require 'todo-comments'
+    local todo_comments_config = require 'todo-comments.config'
+
+    local function set_mark_highlight_override()
+      if vim.o.background ~= 'light' then
+        return
+      end
+
+      local ok, palettes = pcall(require, 'catppuccin.palettes')
+      local colors_ok, colors = pcall(require, 'catppuccin.utils.colors')
+      if not ok or not colors_ok then
+        return
+      end
+
+      local palette = palettes.get_palette()
+      vim.api.nvim_set_hl(0, 'TodoBgMARK', {
+        bg = colors.darken(palette.teal, 0.12, palette.base),
+        fg = palette.teal,
+        bold = true,
+      })
+    end
+
+    local function refresh_todo_comment_highlights()
+      if not todo_comments_config.loaded then
+        return
+      end
+
+      for keyword in pairs(todo_comments_config.options.keywords) do
+        vim.api.nvim_set_hl(0, 'TodoBg' .. keyword, {})
+        vim.api.nvim_set_hl(0, 'TodoFg' .. keyword, {})
+        vim.api.nvim_set_hl(0, 'TodoSign' .. keyword, {})
+      end
+
+      todo_comments_config.colors()
+      set_mark_highlight_override()
+    end
+
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = vim.api.nvim_create_augroup('TodoCommentsThemeFix', { clear = true }),
+      callback = function()
+        vim.defer_fn(refresh_todo_comment_highlights, 20)
+      end,
+    })
 
     vim.api.nvim_create_user_command('Marks', function()
       marks.open_picker()
